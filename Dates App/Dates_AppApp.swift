c0d2900a98924d1813c238898acd7f38efc946ed
agholson <87455899+agholson@users.noreply.github.com>
2022-03-02 22:10:59 -0500

    Initial Commit

---
+++ Dates App/Dates_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dates_AppApp.swift
+//  Dates App
+//
+//  Created by Leone on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Dates_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
